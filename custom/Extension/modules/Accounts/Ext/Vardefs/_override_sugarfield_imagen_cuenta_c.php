834ba086d6f78b7e42500f39c962237b99120ba6
Javier Garcia <jgarcia@keybps.com>
2021-12-21 10:10:59 -0700

    Campo de prueba

---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/_override_sugarfield_imagen_cuenta_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2021-12-21 10:06:45
+$dictionary['Account']['fields']['imagen_cuenta_c']['inline_edit']='1';
+$dictionary['Account']['fields']['imagen_cuenta_c']['labelValue']='Imagen de la cuenta';
+
+ ?>
